d9307c6cbd0e0ba615d1f8143e14f3aa5b74f90f
Adamki <adajensen@gmail.com>
2017-12-21 13:39:33 -0800

    adds ProgressBar comp and examples

---
+++ src/docs/examples/ProgressBar/Example50Percent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ProgressBar from 'ps-react/ProgressBar';
+
+/** 51% progess */
+export default function Example10Percent() {
+  return <ProgressBar width={250} height={30} percent={51} />
+}

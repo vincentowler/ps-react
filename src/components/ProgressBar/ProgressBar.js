4e47f39c4329a2e734ad398424d74bf0aa501449
Amadou Sall <ahasall@yahoo.fr>
2018-11-10 18:04:33 +0100

    [ts] 'percent' is declared but its value is never read

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -3,8 +3,8 @@ import PropTypes from 'prop-types';
 
 class ProgressBar extends React.Component {
   getColor = (percent) => {
-    if (this.props.percent === 100) return 'green';
-    return this.props.percent > 50 ? 'lightgreen' : 'red';
+    if (percent === 100) return 'green';
+    return percent > 50 ? 'lightgreen' : 'red';
   }
 
   getWidthAsPercentOfTotalWidth = () => {
